edb6ba37b9745663391df1f3dc6764f6cd23a59f
Daniel Engelmeier <daniel_engelmeier@hotmail.com>
2020-04-01 16:07:59 -0300

    alteracao do script

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,50 @@
-const cipher = {
-  // ...
-};
+const cipher =  {
+  encode: function (palavra, offset)   {
+    
+    palavra = palavra.toUpperCase();
+    offset = offset % 26;
+    let palavraCodificada = '';
+    
+    for (let contador = 0; contador < palavra.length; contador++)
+    {
+      //este vai ser o numero correspondente a letra na tabela ascii
+      let tabelaAsc = palavra.charCodeAt(contador);
+    
+      //acrescenta o valor do offset no valor da tabela asc
+      tabelaAsc = tabelaAsc + offset;
+    
+      //verifica se o valor da tabela asc passou de 90 (valor da letra z)
+      if(tabelaAsc > 90)
+      {
+        //tirando 26 faz o valor da tabela ascii voltar ao inicio (a)
+        tabelaAsc = tabelaAsc - 26;
+      }
+      //função para retornar o caracter correspondente ao indice passado 
+      palavraCodificada =palavraCodificada+ String.fromCharCode(tabelaAsc);	
+    }
+    
+    return palavraCodificada;
+    
+    
+  },
+  decode: function(palavra, offset){
+
+    palavra = palavra.toUpperCase();
+    offset = offset % 26;
+    let palavraDecodificada = '';
+
+    for (let contador  = 0; contador < palavra.length; contador++) 
+    {
+     let tabelaAsc= palavra.charCodeAt(contador);
+     tabelaAsc= tabelaAsc - offset;
+     if(tabelaAsc<65){
+     tabelaAsc= tabelaAsc+26;
+     }
+     palavraDecodificada= palavraDecodificada+ String.fromCharCode(tabelaAsc);
+    }
+
+    return palavraDecodificada;
+  }
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+let botaoCod= document.getElementById("botaoCod")
+botaoCod.addEventListener("click",function (){
+
+    let senha= document.getElementById("senha");
+    let offset=  document.getElementById("offset");
+    let resultado= cipher.encode(senha.value, offset.value);
+    let resposta= document.getElementById("resposta");
+    resposta.innerHTML= resultado;
+
+});
+
